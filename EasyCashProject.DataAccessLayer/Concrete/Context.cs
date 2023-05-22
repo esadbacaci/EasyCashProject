24306adf83cce5b811c7aec50c3641af382f2d18
Muhammed Esad Bacacı <bcc.esad@gmail.com>
2023-05-22 10:09:23 +0300

    update

---
+++ EasyCashProject.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,21 @@
+﻿using EasyCashProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashProject.DataAccessLayer.Concrete
+{
+    public class Context:IdentityDbContext<AppUser,AppRole ,int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=BCC;initial catalog=EasyCashDb;integrated Security=true");
+        }
+        public DbSet<CustomerAccount> CustomerAccounts { get; set; }
+        public DbSet<CustomerAccountProcess> CustomerAccountProcesses { get; set; }
+    }
+}
